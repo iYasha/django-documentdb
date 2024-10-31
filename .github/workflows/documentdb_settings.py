cefb81396b15f19aeb6cc80976b8d5f060c56909
iYasha <33287747+iYasha@users.noreply.github.com>
2024-10-31 17:15:35 +0200

    feat(tests): Added dockerfile to run tests

--- .github/workflows/documentdb_settings.py
+++ .github/workflows/documentdb_settings.py
@@ -2,13 +2,19 @@ DATABASES = {
     "default": {
         "ENGINE": "django_documentdb",
         "NAME": "djangotests",
+        "HOST": "host.docker.internal",
+        "USER": "root",
+        "PASSWORD": "mongoadmin",
     },
     "other": {
         "ENGINE": "django_documentdb",
         "NAME": "djangotests-other",
+        "HOST": "host.docker.internal",
+        "USER": "root",
+        "PASSWORD": "mongoadmin",
     },
 }
 DEFAULT_AUTO_FIELD = "django_documentdb.fields.ObjectIdAutoField"
 PASSWORD_HASHERS = ("django.contrib.auth.hashers.MD5PasswordHasher",)
-SECRET_KEY = "django_tests_secret_key"
+SECRET_KEY = "django_tests_secret_key"  # noqa: S105
 USE_TZ = False
