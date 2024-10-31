6aa913ad188b48ee2647b1518a4ba39ffc22a9e8
iYasha <33287747+iYasha@users.noreply.github.com>
2024-10-31 18:31:13 +0200

    feat(simple lookup): Added simple lookup operation support using .fin… (#10)
    * feat(simple lookup): Added simple lookup operation support using .find(...)
    
    * feat(simple lookup): removed self.lookup_pipeline from simple lookup
    
    * feat(simple lookup): removed aggregation_pipeline from simple lookups

--- django_documentdb/fields/duration.py
+++ django_documentdb/fields/duration.py
@@ -6,7 +6,7 @@ _get_db_prep_value = DurationField.get_db_prep_value
 def get_db_prep_value(self, value, connection, prepared=False):
     """DurationField stores milliseconds rather than microseconds."""
     value = _get_db_prep_value(self, value, connection, prepared)
-    if connection.vendor == "mongodb" and value is not None:
+    if connection.vendor == "documentdb" and value is not None:
         value //= 1000
     return value
 
