e86cc9574a3fc468076c29302a2f3ccc08bc498b
Ivan <99082173+SavingFrame@users.noreply.github.com>
2025-09-02 11:25:11 +0200

    fix(db): apply fixes from django-mongodb-backend for new django version (#14)
    * Remove usage of non-existent code in the new django versions
    
    * Update version
    
    * Remove index_together support
    
    * Update version
    
    * Fix ordering
    
    * Fix process_lhs based on django-mongodb-backend
    
    * Update version
    
    * Revert version
    
    * Change version
    
    ---------
    
    Co-authored-by: Ivan Symantiev <33287747+iYasha@users.noreply.github.com>

--- django_documentdb/schema.py
+++ django_documentdb/schema.py
@@ -29,7 +29,7 @@ class DatabaseSchemaEditor(BaseDatabaseSchemaEditor):
 
     def _create_model_indexes(self, model):
         """
-        Create all indexes (field indexes & uniques, Meta.index_together,
+        Create all indexes (field indexes & uniques,
         Meta.unique_together, Meta.constraints, Meta.indexes) for the model.
         """
         if not model._meta.managed or model._meta.proxy or model._meta.swapped:
@@ -40,9 +40,6 @@ class DatabaseSchemaEditor(BaseDatabaseSchemaEditor):
                 self._add_field_index(model, field)
             elif self._field_should_have_unique(field):
                 self._add_field_unique(model, field)
-        # Meta.index_together (RemovedInDjango51Warning)
-        for field_names in model._meta.index_together:
-            self._add_composed_index(model, field_names)
         # Meta.unique_together
         if model._meta.unique_together:
             self.alter_unique_together(model, [], model._meta.unique_together)
